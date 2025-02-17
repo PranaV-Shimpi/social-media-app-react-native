e6da86556f8908b96940980c848c564a950be0d0
PranaV Shimpi <23pranavshimpi@gmail.com>
2025-02-17 17:34:55 +0530

    add: welcome page created

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,10 @@
 import { View, Text, Button } from "react-native";
 import React from "react";
 import ScreenWrapper from "../components/ScreenWrapper";
+import { useRouter } from "expo-router";
 
 const index = () => {
+  const router = useRouter()
   return (
     <ScreenWrapper>
       <Text>index</Text>
