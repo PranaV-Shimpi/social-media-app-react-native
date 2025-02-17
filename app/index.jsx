330de43bf17b3e3af506e5c149ae5b52603797cf
PranaV Shimpi <23pranavshimpi@gmail.com>
2025-02-17 12:51:02 +0530

    Initial commit: cleanup code, hello world

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+import ScreenWrapper from "../components/ScreenWrapper";
+
+const index = () => {
+  return (
+    <ScreenWrapper>
+      <Text>index</Text>
+      <Button title="welcome" onPress={() => router.push("welcome")} />
+    </ScreenWrapper>
+  );
+};
+
+export default index;
